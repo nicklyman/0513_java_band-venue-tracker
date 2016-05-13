bd25aabe2c64e1afc9af0484549fef9dbe46486e
Nick Lyman <nick.lyman@gmail.com>
2016-05-13 09:15:53 -0700

    Pass Venue_instantiatesCorrectly_true BDD test to verify that an instance of the Venue object is created correctly.

---
+++ src/main/java/Venue.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import org.sql2o.*;
+import java.util.ArrayList;
+
+public class Venue {
+  private int id;
+  private String venue_name;
+
+  public Venue(String name) {
+    this.venue_name = name;
+  }
+}
---
+++ src/test/java/VenueTest.java
@@ -0,0 +1,18 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class VenueTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Venue_instantiatesCorrectly_true() {
+    Venue myVenue = new Venue("Edgefield");
+    assertEquals(true, myVenue instanceof Venue);
+  }
+}
